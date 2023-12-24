fa2e5f08e5d7705e02cc8ffd3bcdd41bc37e11bd
Pallavi Soam <pallavisoam35@gmail.com>
2023-12-24 19:48:09 +0530

    cors headers added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./index.css";
 import React, { useEffect } from "react";
-
 import { Switch, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -9,7 +8,6 @@ import Register from "./pages/auth/Register";
 import Home from "./pages/Home";
 import Header from "./components/nav/Header";
 import RegisterComplete from "./pages/auth/RegisterComplete";
-
 import ForgotPassword from "./pages/auth/ForgotPassword";
 import { auth } from "./firebase";
 import { useDispatch } from "react-redux";
